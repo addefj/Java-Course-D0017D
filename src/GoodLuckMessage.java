01cb89b2475c94da35b8b41598679eaa76c5b9e1
addefj <adde.fj@gmail.com>
2025-02-19 10:42:13 +0100

    Initial commit, prints Good Luck!

---
+++ src/GoodLuckMessage.java
@@ -0,0 +1,12 @@
+/**
+ * Describe briefly your program in steps.
+ *Use System.out.println() to print Hello World!
+ * @author Your Name (LTU_username)
+ * @version 1.0
+ */
+public class GoodLuckMessage {
+    public static void main(final String[] args)
+        {
+        System.out.println("Good Luck!");
+    }
+}
