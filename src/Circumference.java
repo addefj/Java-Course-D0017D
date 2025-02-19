a2345882f1338b3dd409333007e8d60a9d463350
addefj <adde.fj@gmail.com>
2025-02-19 12:24:09 +0100

    initial commit, calculates circumference

---
+++ src/Circumference.java
@@ -0,0 +1,12 @@
+public class Circumference {
+    public static void main(final String[] args)
+        {
+
+            final float Pi = 3.14159f;
+            final int diameter = 2;
+
+            float circumference = Pi * diameter;
+                    System.out.print("The circumference for the circle is: " + circumference + " cm");
+                    //System.out.print(circumference);
+        }
+}
