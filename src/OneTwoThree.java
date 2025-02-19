7f471f736e66d80a85af91839db206a9750a6e72
addefj <adde.fj@gmail.com>
2025-02-19 11:20:30 +0100

    initial commit, prints One, two, three on seperate lines

---
+++ src/OneTwoThree.java
@@ -0,0 +1,9 @@
+public class OneTwoThree {
+    public static void main(final String[] args)
+        {
+        //System.out.print("one");
+        //System.out.print("two");
+        //System.out.println("three");
+        System.out.println("one \ntwo \nthree");
+    }
+}
