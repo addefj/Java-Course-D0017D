0c1b44b423668fa0a845caeb81e939dd01775d1d
addefj <adde.fj@gmail.com>
2025-02-19 12:22:53 +0100

    initial commit, for loop with displayed message

---
+++ src/LoveEverywhere.java
@@ -0,0 +1,22 @@
+//import scanner class
+
+//import class Main
+public class LoveEverywhere {
+
+    public static void main(final String[] args) {
+
+  //      int i = 0;
+/*
+        while (i++ < 5) {
+            System.out.println("love, love everywhere");
+            i++;
+        }
+*/
+        for (int i = 0; i < 5; i++ ) {
+            System.out.println("love, love everywhere");
+        }
+
+
+
+    }
+}
